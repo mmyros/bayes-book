30a243a96c40f2e3fc0b6c58f08fc16181050944
m <mmyros@gmail.com>
2021-11-12 11:54:55 -0500

    test data

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for bayes_window_examples."""
---
+++ tests/test_bayes_window_examples.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Tests for `bayes_window_examples` package."""
+
+import pytest
+
+
+from bayes_window_examples import bayes_window_examples
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
